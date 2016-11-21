bcc78fb772e09e204d709d618535af72c738270b
avgarm <avgarm@wix.com>
2016-11-21 15:33:56 +0200

    use store in todoList

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -6,15 +6,35 @@ import todos from './../../reducers/todos.js'
 
 class TodoList extends Component {
 
+    componentDidMount () {
+        const { store } = this.context
+        this.unsubscribe = store.subscribe(() => {
+            this.forceUpdate()
+        })
+    }
 
+    componentWillUnmount () {
+        this.unsubscribe()
+    }
 
     render () {
+        const { store } = this.context
+        console.log('**************************')
+        console.log(this.context)
+        console.log(store.getState())
+
         var tasks =
-            _.map(this.props.tasks, (t) => <li key={t.id} className="task">{t.text}</li>)
-        return <ul>
-            {tasks}
-        </ul>
+            _.map(store.getState().todos, (t) => <li key={t.id} className="task">{t.text}</li>)
+        return <div>
+            <input />
+            <button onClick={()=> {store.dispatch({id:0, type: 'ADD_TODO', text: 'task1'})}}>Add</button>
+            <ul>
+                {tasks}
+            </ul>
+            </div>
     }
 }
-
+TodoList.contextTypes = {
+    store: React.PropTypes.object
+}
 export default TodoList;
